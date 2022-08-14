b1404660eb3bc93fe82370b5cd52439b3664e9e3
Miłosz Mertka <milosz.mertka@onet.pl>
2022-08-14 22:07:37 +0200

    initial commit

---
+++ src/main/java/com/milmertka/beesocial/BeesocialApplication.java
@@ -0,0 +1,13 @@
+package com.milmertka.beesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeesocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeesocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milmertka/beesocial/BeesocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milmertka.beesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeesocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
