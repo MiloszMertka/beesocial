cc69a7ab0cacf7c5db11f2428355e2e6db7a4659
Miłosz Mertka <milosz.mertka@onet.pl>
2022-08-16 19:10:05 +0200

    Created UserRepositoryTest

---
+++ src/test/java/com/milmertka/beesocial/user/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.milmertka.beesocial.user;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void findByEmail() {
+        // given
+        User user = new User("test@beesocial.com", "John", "Doe", "password123");
+        userRepository.save(user);
+
+        // when
+        Optional<User> foundUser = userRepository.findByEmail(user.getEmail());
+
+        // then
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get()).isEqualTo(user);
+    }
+}
